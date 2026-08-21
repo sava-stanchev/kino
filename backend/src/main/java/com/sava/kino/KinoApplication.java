b6f4c1e32fed206581eb45c72483572c1c910784
sava-stanchev <s.stanchev94@gmail.com>
2026-08-21 20:19:21 +0300

    set up spring boot and postgres

---
+++ backend/src/main/java/com/sava/kino/KinoApplication.java
@@ -0,0 +1,13 @@
+package com.sava.kino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/sava/kino/KinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sava.kino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
