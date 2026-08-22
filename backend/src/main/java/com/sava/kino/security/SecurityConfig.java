53af156ebd655cc39a1de1524215f937df855e8f
sava-stanchev <s.stanchev94@gmail.com>
2026-08-22 23:28:39 +0300

    configure cors

--- backend/src/main/java/com/sava/kino/security/SecurityConfig.java
+++ backend/src/main/java/com/sava/kino/security/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
+        http.cors(cors -> {}).authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.GET, "/api/movies").permitAll().anyRequest().authenticated());
         return http.build();
     }
