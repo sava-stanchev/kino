7292b312a84e2d1a39d7ac087c6e5c7899686a20
sava-stanchev <s.stanchev94@gmail.com>
2026-08-22 15:24:06 +0300

    review entity

---
+++ backend/src/main/java/com/sava/kino/review/Review.java
@@ -0,0 +1,34 @@
+package com.sava.kino.review;
+
+import com.sava.kino.movie.Movie;
+import com.sava.kino.user.User;
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @Column(nullable = false, length = 5000)
+    private String content;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+    protected Review(){}
+}
