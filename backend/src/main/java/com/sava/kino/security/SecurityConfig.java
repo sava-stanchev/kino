e2faf553107b30d42ade9db0908b56e641aa11dd
sava-stanchev <s.stanchev94@gmail.com>
2026-08-29 15:21:44 +0300

    auth manager

--- backend/src/main/java/com/sava/kino/security/SecurityConfig.java
+++ backend/src/main/java/com/sava/kino/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.sava.kino.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,5 +27,10 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
